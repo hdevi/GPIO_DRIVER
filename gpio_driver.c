9703dfc212cf9eadb649351ff26b2c5690a9f552
Demo User <ubuntu@arm.localdomain>
2018-12-27 06:47:45 +0000

    driver init/exit

--- gpio_driver.c
+++ gpio_driver.c
@@ -1,1 +1,29 @@
 #include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/fs.h>
+#include<linux/device.h>
+
+static struct fileoperations led_ops = 
+{
+	.owner = THIS_MODULE,
+	.open = led_open,
+	.read = led_read,
+	.write = led_write,
+	.release = led_close,
+};
+
+static int __init gpio_init(void)
+{
+	printk(KERN_INFO "Initializing GPIO Driver\n");
+	return 0;
+}
+
+static int __exit gpio_exit(void)
+{
+	printk(KERN_INFO "Exiting GPIO Driver\n");
+	return 0;
+}
+
+module_init(gpio_init);
+module_exit(gpio_exit);
